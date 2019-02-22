c20613668d608def7471b4ac084a59d4e02c1263
shaiTheGuy <shay.fridboim@gmail.com>
2019-02-22 05:06:18 -0600

    Update images.js

--- src/data/images.js
+++ src/data/images.js
@@ -11,6 +11,7 @@ export const colors = {
 }
 
 export default [
+    /*
     {
         url: "https://images.unsplash.com/photo-1460574283810-2aab119d8511?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=1344&q=80",
         dominantColors: [
@@ -103,7 +104,8 @@ export default [
             colors.grey
         ]
     },
-    /*{
+    */
+    {
         url: "images/1.png",
         dominantColors: [
             colors.purple,
@@ -175,8 +177,694 @@ export default [
         url: "images/10.png",
         dominantColors: [
             colors.purple,
+            colors.green,
+            colors.yellow
+        ]
+    },
+        {
+        url: "images/11.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/12.jpg",
+        dominantColors: [
+            colors.green,
+            colors.red,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/13.jpg",
+        dominantColors: [
+            colors.orange,
+            colors.yellow,
+            colors.red  
+        ]
+    },
+    {
+        url: "images/14.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.purple
+        ]
+    },
+    {
+        url: "images/15.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green
+        ]
+    },
+    {
+        url: "images/16.jpeg",
+        dominantColors: [
+            colors.purple,
+            colors.green,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/17.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.red,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/18.jpg",
+        dominantColors: [
+            colors.green,
+            colors.blue,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/19.jpg",
+        dominantColors: [
+            colors.red,
+            colors.purple
+        ]
+    },
+    {
+        url: "images/20.jpeg",
+        dominantColors: [
+            colors.red,
+            colors.blue
+        ]
+    },
+        {
+        url: "images/21.jpg",
+        dominantColors: [
+            colors.green,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/22.jpeg",
+        dominantColors: [
+            colors.orange,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/23.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/24.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green
+        ]
+    },
+    {
+        url: "images/25.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/26.jpg",
+        dominantColors: [
+            colors.green,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/27.jpg",
+        dominantColors: [
+            colors.green,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/28.jpg",
+        dominantColors: [
+            colors.pink,
+            colors.purple,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/29.gif",
+        dominantColors: [
+            colors.yellow,
+            colors.green,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/30.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.blue
+        ]
+    },
+        {
+        url: "images/31.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.pink,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/32.jpg",
+        dominantColors: [
+            colors.red,
+            colors.yellow,
+            colors.green,
+            colors.blue,
+            colors.purple
+        ]
+    },
+    {
+        url: "images/33.jpg",
+        dominantColors: [
+            colors.pink,
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/34.png",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/35.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.yellow,
+            colors.pink,
+            colors.green
+        ]
+    },
+    {
+        url: "images/36.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/37.jpg",
+        dominantColors: [
+            colors.orange,
+            colors.green
+        ]
+    },
+    {
+        url: "images/38.jpg",
+        dominantColors: [
+            colors.green,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/39.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.pink,
+            colors.purple
+        ]
+    },
+    {
+        url: "images/40.png",
+        dominantColors: [
+            colors.blue,
+            colors.green,
+            colors.orange,
+            colors.red
+        ]
+    },
+        {
+        url: "images/41.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/42.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.purple,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/43.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.red,
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/44.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.red
+        ]
+    },
+    {
+        url: "images/45.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green,
+            colors.pink,
+            colors.purple,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/46.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.green,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/47.jpg",
+        dominantColors: [
+            colors.green,
+            colors.red
+        ]
+    },
+    {
+        url: "images/48.jpg",
+        dominantColors: [
+            colors.green,
+            colors.orange,
+            colors.yellow,
+            colors.red
+        ]
+    },
+    {
+        url: "images/49.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/50.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.orange,
+            colors.red
+        ]
+    },
+        {
+        url: "images/51.jpg",
+        dominantColors: [
+            colors.pink,
+            colors.orange,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/52.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.red
+        ]
+    },
+    {
+        url: "images/53.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green
+        ]
+    },
+    {
+        url: "images/54.jpg",
+        dominantColors: [
+            colors.orange,
+            colors.red
+        ]
+    },
+    {
+        url: "images/55.jpg",
+        dominantColors: [
+            colors.red,
+            colors.yellow,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/56.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/57.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/58.jpg",
+        dominantColors: [
+            colors.green,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/59.jpg",
+        dominantColors: [
+            colors.red,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/60.jpg",
+        dominantColors: [
+            colors.green,
+            colors.yellow
+        ]
+    },
+        {
+        url: "images/61.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.orange,
+            colors.red
+        ]
+    },
+    {
+        url: "images/62.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.blue,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/63.jpg",
+        dominantColors: [
+            colors.green,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/64.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/65.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/66.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.pink,
+            colors.red
+        ]
+    },
+    {
+        url: "images/67.jpg",
+        dominantColors: [
+            colors.green,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/68.jpg",
+        dominantColors: [
+            colors.red,
+            colors.blue,
+            colors.purple
+        ]
+    },
+    {
+        url: "images/69.jpg",
+        dominantColors: [
             colors.pink,
             colors.yellow
         ]
-    }*/
+    },
+    {
+        url: "images/70.jpg",
+        dominantColors: [
+            colors.pink,
+            colors.yellow,
+            colors.green,
+            colors.red
+        ]
+    },
+        {
+        url: "images/71.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/72.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/73.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/74.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/75.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green
+        ]
+    },
+    {
+        url: "images/76.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.green,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/77.jpeg",
+        dominantColors: [
+            colors.blue,
+            colors.red,
+            colors.yellow
+        ]
+    },
+    {
+        url: "images/78.jpeg",
+        dominantColors: [
+            colors.blue,
+            colors.yellow,
+            colors.red
+        ]
+    },
+    {
+        url: "images/79.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.purple,
+            colors.orange
+        ]
+    },
+    {
+        url: "images/80.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.blue,
+            colors.green
+        ]
+    },
+        {
+        url: "images/81.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.orange,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/82.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.blue,
+            colors.red
+        ]
+    },
+    {
+        url: "images/83.jpg",
+        dominantColors: [
+            colors.red,
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/84.jpg",
+        dominantColors: [
+            colors.green,
+            colors.red
+        ]
+    },
+    {
+        url: "images/85.jpeg",
+        dominantColors: [
+            colors.blue,
+            colors.green
+        ]
+    },
+    {
+        url: "images/86.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.orange,
+            colors.red
+        ]
+    },
+    {
+        url: "images/87.jpg",
+        dominantColors: [
+            colors.orange,
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/88.jpg",
+        dominantColors: [
+            colors.red,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/89.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green,
+            colors.blue,
+            colors.yellow,
+            colors.purple,
+            colors.orange,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/90.jpg",
+        dominantColors: [
+            colors.red,
+            colors.pink
+        ]
+    },
+        {
+        url: "images/91.jpg",
+        dominantColors: [
+            colors.pink,
+            colors.green
+        ]
+    },
+    {
+        url: "images/92.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.red,
+            colors.green
+        ]
+    },
+    {
+        url: "images/93.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.yellow,
+            colors.red
+        ]
+    },
+    {
+        url: "images/94.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.blue
+        ]
+    },
+    {
+        url: "images/95.jpg",
+        dominantColors: [
+            colors.green,
+            colors.pink
+        ]
+    },
+    {
+        url: "images/96.jpg",
+        dominantColors: [
+            colors.blue,
+            colors.yellow,
+            colors.red
+        ]
+    },
+    {
+        url: "images/97.jpg",
+        dominantColors: [
+            colors.red,
+            colors.green
+        ]
+    },
+    {
+        url: "images/98.jpg",
+        dominantColors: [
+            colors.green,
+            colors.yellow,
+            colors.red,
+            colors.blue,
+            colors.purple
+        ]
+    },
+    {
+        url: "images/99.jpg",
+        dominantColors: [
+            colors.yellow,
+            colors.green
+        ]
+    },
+    {
+        url: "images/100.jpg",
+        dominantColors: [
+            colors.purple,
+            colors.yellow
+        ]
+    }
 ];
